5259910eee5b82318aab3c7d59976b1257470572
Thanarit Kanjanamatawat <u6410322@au.edu>
2024-09-04 22:47:55 +0700

    Make Getting environmental variable applicable on Vercel

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fasthtml.common import *
 from dotenv import load_dotenv
 
 load_dotenv()
-TESVAR = os.getenv("TESTVAR")
+TESTVAR = os.environ.get("TESTVAR")
+
 app, rt = fast_app()
 # This is test Comment
 
 @rt('/')
 
 def get():
-    return Div(P(f"Hello World! {TESVAR}"), hx_get="/change")
+    return Div(P(f"Hello World! {TESTVAR}"), hx_get="/change")
 
 
 
